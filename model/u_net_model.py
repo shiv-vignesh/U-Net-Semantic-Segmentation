32a5f9b3fc1bfad4e2bca17d975b32afa35aa487
Shiv Murthy <sm2678@rit.edu>
2024-04-13 15:32:06 -0400

    modified

--- model/u_net_model.py
+++ model/u_net_model.py
@@ -36,7 +36,41 @@ class SimpleConvBlock(nn.Module):
 
     def forward(self, x):
         return self.convblock(x)
-    
+
+class ResidualConvBlock(nn.Module):
+
+    def __init__(self, in_channels:int, out_channels:int, kernel_size:int=3, padding=1, add_dropout:bool=False, add_maxpool:bool=False):
+        super(ResidualConvBlock, self).__init__()
+
+        self.add_maxpool = add_maxpool
+        self.add_dropout = add_dropout
+
+        self.convblock_1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=True)
+        self.convblock_2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=True)
+        
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+        self.dropout = nn.Dropout()
+
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+            
+    def forward(self, x):
+
+        conv1_out = self.convblock_1(x)
+        out = self.convblock_2(conv1_out)
+
+        out = self.batch_norm(out)
+        out = self.relu(out)
+
+        out = out + conv1_out
+
+        if self.add_maxpool:
+            out = self.max_pool(out) 
+
+        return self.relu(out)
+
 class SimpleDeConvBlock(nn.Module):
 
     def __init__(self, in_channels:int, out_channels:int) -> None:
@@ -45,7 +79,6 @@ class SimpleDeConvBlock(nn.Module):
         #out = (x - 1)s - 2p + d(k - 1) + op + 1
         self.transpose_layer = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2, padding=0)
         self.convblock = SimpleConvBlock(in_channels, out_channels)
-        # self.convblock = nn.Conv2d(in_channels, out_channels)
 
     def forward(self, input_x:torch.tensor, concat_x:torch.tensor):
 
@@ -57,29 +90,24 @@ class SimpleDeConvBlock(nn.Module):
 
         return deconv_output
 
-class SimpleDeConvBlock_Revised(nn.Module):
+class ResidualDeConvBlock(nn.Module):
 
     def __init__(self, in_channels:int, out_channels:int) -> None:
-        super(SimpleDeConvBlock_Revised, self).__init__()
+        super(ResidualDeConvBlock, self).__init__()
 
         #out = (x - 1)s - 2p + d(k - 1) + op + 1
         self.transpose_layer = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2, padding=0)
-        self.convblock = SimpleConvBlock(in_channels, out_channels)
-        # self.convblock = nn.Conv2d(in_channels, out_channels, )
+        self.convblock = ResidualConvBlock(in_channels, out_channels)
 
     def forward(self, input_x:torch.tensor, concat_x:torch.tensor):
 
         transpose_output = self.transpose_layer(input_x)
-        croped = self.crop(concat_x, transpose_output)
-        concatenated_output = torch.concat([transpose_output, croped], dim=1)
+        transpose_output = nn.ReLU()(transpose_output)
+
+        concatenated_output = torch.concat([transpose_output, concat_x], dim=1)
         deconv_output = self.convblock(concatenated_output)
 
         return deconv_output
-
-    def crop(self,input_tensor,target_tensor):
-        # For making the size of the encoder conv layer and the decoder Conv layer same
-        _,_,H,W = target_tensor.shape
-        return transform.CenterCrop([H,W])(input_tensor)
 
 class AttentionGate(nn.Module):
 
@@ -91,7 +119,7 @@ class AttentionGate(nn.Module):
         x - upper layer feature map (has lesser channels). (skip connection from parallel encoder layer)
         '''
 
-        self.g_conv_layer = nn.Conv2d(g, x, kernel_size=1) #(n_c, h, w) -> (m_c, h', w')
+        self.g_conv_layer = nn.Conv2d(g, x, kernel_size=1) #(n_c, h, w) -> (m_c, h, w)
         self.x_conv_layer = nn.Conv2d(x, x, kernel_size=3, stride=2, padding=1) #(n_c, h', w') -> (n_c, h, w)
 
         self.attention_layer = nn.Conv2d(x, 1, kernel_size=1)
@@ -133,9 +161,11 @@ class UNet(nn.Module):
 
         for i, n_channel in enumerate(self.channels):
             if n_channel == self.n_channel_layer_6:
-                convblock = SimpleConvBlock(in_channels=input_channels, out_channels=n_channel, add_maxpool=False)
+                # convblock = SimpleConvBlock(in_channels=input_channels, out_channels=n_channel, add_maxpool=False)
+                convblock = ResidualConvBlock(in_channels=input_channels, out_channels=n_channel, add_maxpool=False)
             else:             
-                convblock = SimpleConvBlock(in_channels=input_channels, out_channels=n_channel, add_dropout=True)
+                # convblock = SimpleConvBlock(in_channels=input_channels, out_channels=n_channel, add_dropout=True)
+                convblock = ResidualConvBlock(in_channels=input_channels, out_channels=n_channel, add_dropout=True)
             
             self.encoder_module.add_module(name=f'convblock_{i}', module=convblock)
             input_channels = n_channel
@@ -146,8 +176,8 @@ class UNet(nn.Module):
 
         for i, n_channel in enumerate(self.channels[-2::-1]):
             attn_gate = AttentionGate(input_channels, n_channel)
-            deconvblock = SimpleDeConvBlock(in_channels=input_channels, out_channels=n_channel)
-            # deconvblock = SimpleDeConvBlock_Revised(in_channels=input_channels, out_channels=n_channel)
+            # deconvblock = SimpleDeConvBlock(in_channels=input_channels, out_channels=n_channel)
+            deconvblock = ResidualDeConvBlock(in_channels=input_channels, out_channels=n_channel)
             self.decoder_module.add_module(name=f'deconvblock_{i}', module=deconvblock)
             self.attention_gate_module.add_module(name=f'attn_gate_{i}', module=attn_gate)
             input_channels = n_channel
@@ -158,7 +188,7 @@ class UNet(nn.Module):
         self.final_classification_layer = nn.Conv2d(n_channel, output_classes, kernel_size=1, stride=1)
 
 
-    def forward(self, image_tensors:torch.tensor, label_tensors:torch.tensor):
+    def forward(self, image_tensors:torch.tensor, label_tensors:torch.tensor, region_masks:torch.tensor=None):
 
         encoder_features = []
         
@@ -182,33 +212,35 @@ class UNet(nn.Module):
         
         predicted_segmentation_map = self.pre_classification_layer(output_features)
         predicted_segmentation_map = self.final_classification_layer(output_features)
-
-        # if predicted_segmentation_map.shape[2:] != label_tensors.shape[2:]:
-        #     predicted_segmentation_map = F.interpolate(predicted_segmentation_map, list(label_tensors.shape)[1:])
-
-        # predicted_segmentation_map = nn.Softmax(dim=1)(predicted_segmentation_map)     
-
-        # predicted_segmentation_map = torch.argmax(predicted_segmentation_map, dim=1)   
         
-        # cross_loss = nn.CrossEntropyLoss(ignore_index=0)(
-        #     predicted_segmentation_map, label_tensors.squeeze(1).long()
-        # )
+        cross_loss = nn.CrossEntropyLoss(ignore_index=0)(
+            predicted_segmentation_map, label_tensors.squeeze(1).long()
+        )
 
-        # focal_loss = calculate_focal_loss(cross_loss)
+        if region_masks is not None:
+            ''' 
+            TODO- Recheck how to formulate masked loss. 
+            '''
+            per_pixel_loss = F.cross_entropy(predicted_segmentation_map, label_tensors.squeeze(1).long(), reduction="none")
+            masked_loss = per_pixel_loss * region_masks
+            # print(masked_loss.size())
+            # print(masked_loss.mean())
+            
+            if masked_loss.numel() > 0:  # Check if there are any elements to avoid division by zero
+                #only compute mean for elements with 1s
+                masked_loss = torch.masked_select(per_pixel_loss, region_masks.bool()).mean()
+            else:
+                masked_loss = torch.tensor(0.0, device=predicted_segmentation_map.device, requires_grad=True)            
 
-        dice_loss = calculate_dice_loss(
-            predicted_segmentation_map, label_tensors, self.output_classes
-        )        
+            #alpha = 0.5
+            #alpha*(1-alpha)
+            total_loss = cross_loss + 0.5 * masked_loss
 
-        return dice_loss, predicted_segmentation_map, label_tensors
+        else:
+            total_loss = cross_loss
 
-        # iou_loss = calculate_soft_iou_loss(
-        #     predicted_segmentation_map, label_tensors, self.output_classes
-        # )
+        return total_loss, predicted_segmentation_map, label_tensors
 
-        # loss = calculate_focal_loss(
-        #     predicted_segmentation_map, label_tensors
-        # )
 
             
 
